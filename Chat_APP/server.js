3d3fffb5d9ea76281dfcfc53b9ba0b2399d64451
HITESH MEWADA <mewadahitesh00@gmail.com>
2023-01-14 17:22:35 +0530

    Log Updated

--- Chat_APP/server.js
+++ Chat_APP/server.js
@@ -7,7 +7,7 @@ const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT , () =>{
-    console.log(`Listening on PORT ${PORT}`)
+    console.log(`Listening on PORT http://localhost:${PORT}`)
 } )
 
 app.use(express.static(__dirname + '/public'))
